5c2d95feadc635eb7760547c681d66613fd3ec65
Henrike Weingärtner <henrike.honig@gmail.com>
2019-10-02 09:25:55 +0200

    changed maps to tracks

--- JustATest/app/src/main/java/com/example/ARMap/DisplaySavedTracks.java
+++ JustATest/app/src/main/java/com/example/ARMap/DisplaySavedTracks.java
@@ -9,20 +9,20 @@ import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class DisplaySavedMaps extends AppCompatActivity {
+public class DisplaySavedTracks extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saved_maps);
+        setContentView(R.layout.activity_saved_tracks);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton maps_button = findViewById(R.id.maps_button);
-        maps_button.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton tracks_button = findViewById(R.id.tracks_button);
+        tracks_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "There are no maps to add", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "There are no tracks to add", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
